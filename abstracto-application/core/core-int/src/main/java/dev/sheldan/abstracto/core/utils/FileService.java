dd975032387f15e535cfe45bfbdbd1f9456fd7f5
Sheldan <5037282+Sheldan@users.noreply.github.com>
2021-04-06 21:11:36 +0200

    [AB-172] updating to JDA 4.2.1_254 and removing deprecated code from guava

--- abstracto-application/core/core-int/src/main/java/dev/sheldan/abstracto/core/utils/FileService.java
+++ abstracto-application/core/core-int/src/main/java/dev/sheldan/abstracto/core/utils/FileService.java
@@ -1,14 +1,17 @@
 package dev.sheldan.abstracto.core.utils;
 
-import com.google.common.io.Files;
+import dev.sheldan.abstracto.core.exception.AbstractoRunTimeException;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FileUtils;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
 
 @Component
+@Slf4j
 public class FileService {
     public void writeContentToFile(File file, String content) throws IOException {
         try(FileWriter fw = new FileWriter(file)) {
@@ -21,8 +24,13 @@ public class FileService {
         FileUtils.writeByteArrayToFile(file, content);
     }
 
-    public File createTempFile(String fileName) {
-        return new File(Files.createTempDir(), fileName);
+    public File createTempFile(String fileName)  {
+        try {
+            return new File(Files.createTempDirectory("").toFile(), fileName);
+        } catch (IOException e) {
+            log.error("Failed to create temporary file.");
+            throw new AbstractoRunTimeException(e);
+        }
     }
 
     public void safeDelete(File file) throws IOException {
